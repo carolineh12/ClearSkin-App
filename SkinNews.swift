4fc469aa4e912bcae8eb63358ff08eb9ab83b374
michelle1510 <141178978+michelle1510@users.noreply.github.com>
2024-02-21 11:40:00 -0500

    Update SkinNews.swift

--- SkinNews.swift
+++ SkinNews.swift
@@ -12,87 +12,104 @@ struct NewsView: View {
     @State private var stuff = SkinStuff.examples()
     @State private var news = MedicalNews.examples()
     @State private var collection = Collection.examples()
+    @State private var searchTerm = ""
+    
+    var filteredStuff: [SkinStuff] {
+        guard !searchTerm.isEmpty else {return stuff}
+        return stuff.filter {
+            $0.name.localizedCaseInsensitiveContains(searchTerm)
+        }
+    }
+    
+    var filteredNews: [MedicalNews] {
+        guard !searchTerm.isEmpty else {return news}
+        return news.filter {
+            $0.name.localizedCaseInsensitiveContains(searchTerm)
+        }
+    }
+    
+    var filteredCollection: [Collection] {
+        guard !searchTerm.isEmpty else {return collection}
+        return collection.filter {
+            $0.name.localizedCaseInsensitiveContains(searchTerm)
+        }
+    }
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVStack {
-                    HStack {
-                        NavigationLink("Search Topic") {}
-                            .padding(4)
-                            .font(.headline)
-                            .frame(maxWidth: 400, alignment: .center)
-                            .padding(2)
-                            .foregroundStyle(.gray)
-                            .border(.black)
-                            .frame(alignment: .trailing)
-                        Spacer()
-                    }
-                    .padding(15)
-                    
-                    
-                    HStack {
-                        Text("New Products")
-                            .bold()
-                            .font(.title2)
-                            .multilineTextAlignment(.leading)
-                            .padding([.leading])
-                        Spacer()
-                    }
-                    
-                    ScrollView(.horizontal) {
-                        LazyHStack {
-                            ForEach(stuff) {stuff in
-                                SkinStuffCardView(stuff: stuff)
+        NavigationStack {
+            NavigationView {
+                ScrollView {
+                    LazyVStack {
+                        HStack {
+                            Text("")
+                                .padding(2)
+                        }
+                        HStack {
+                            Text("New Products")
+                                .bold()
+                                .font(.title2)
+                                .multilineTextAlignment(.leading)
+                                .padding([.leading])
+                            Spacer()
+                        }
+                        
+                        ScrollView(.horizontal) {
+                            LazyHStack {
+                                ForEach(filteredStuff) {stuff in
+                                    SkinStuffCardView(stuff: stuff)
+                                }
                             }
                         }
-                    }
-                    .padding(10)
-                    .frame(height: 200)
-                    .navigationTitle("Skin News")
-                    
-                    HStack {
-                        Text("Medical News")
-                            .font(.title)
-                            .bold()
-                            .multilineTextAlignment(.leading)
-                            .padding([.leading])
-                        Spacer()
-                    }
-                    
-                    ScrollView(.horizontal) {
-                        LazyHStack {
-                            ForEach(news) {news in
-                                MedicalNewsCardView(news: news)
-                                    .padding([.bottom, .trailing], 10)
-                                    .padding([.leading, .leading], 20)
+                        .padding(10)
+                        .frame(height: 200)
+                        
+                        HStack {
+                            Text("Medical News")
+                                .font(.title)
+                                .bold()
+                                .multilineTextAlignment(.leading)
+                                .padding([.leading])
+                            Spacer()
+                        }
+                        
+                        ScrollView(.horizontal) {
+                            LazyHStack {
+                                ForEach(filteredNews) {news in
+                                    MedicalNewsCardView(news: news)
+                                        .padding([.bottom, .trailing], 10)
+                                        .padding([.leading, .leading], 20)
+                                }
                             }
                         }
-                    }
-                    .padding(10)
-                    .frame(height: 250)
-                    
-                    HStack {
-                        Text("Collection")
-                            .bold()
-                            .font(.title2)
-                            .multilineTextAlignment(.leading)
-                            .padding([.leading])
-                        Spacer()
-                    }
-                    
-                    ScrollView(.horizontal) {
-                        LazyHStack {
-                            ForEach(collection) {collection in
-                                CollectionCardView(collection: collection)
+                        .padding(10)
+                        .frame(height: 250)
+                        
+                        HStack {
+                            Text("Collection")
+                                .bold()
+                                .font(.title2)
+                                .multilineTextAlignment(.leading)
+                                .padding([.leading])
+                            Spacer()
+                        }
+                        
+                        ScrollView(.horizontal) {
+                            LazyHStack {
+                                ForEach(filteredCollection) {collection in
+                                    CollectionCardView(collection: collection)
+                                }
                             }
                         }
+                        .padding(10)
+                        .frame(height: 200)
                     }
-                    .padding(10)
-                    .frame(height: 200)
                 }
             }
+            .navigationTitle("Skin News")
+            .searchable(text: $searchTerm, prompt: "Search Topic")
+
         }
+      //  .navigationTitle("Skin News")
     }
 }
 
@@ -101,5 +118,4 @@ struct NewsView_Previews: PreviewProvider {
         NewsView()
     }
 }
-
 
